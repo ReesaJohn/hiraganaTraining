663cc4fe7b32d718b7152c8f35d6fec424be7730
ReesaJohn <reesamariamjohn@gmail.com>
2019-09-08 16:23:26 -0700

    Update create_folders.py

--- create_folders.py
+++ create_folders.py
@@ -11,12 +11,8 @@ def create_folders(path):
     for i in os.listdir(path):
         current_folder = getFolderName(i)
         if os.path.exists(current_folder):
-            print(path + "/" +i)
-            print(current_folder+"/"+i)
             shutil.copy( path + "/" +i,current_folder+"/"+i)
         else:
             os.mkdir(current_folder)
-            print(path + "/" + i)
-            print(current_folder + "/" + i)
             shutil.copy(path + "/" + i, current_folder + "/" + i)
 create_folders("hiragana_images")
